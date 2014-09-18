5a6ff0f3bdc82e1e0f89a6a8279a5b5d52a3f060
Juliet <junder@bk.ru>
2014-09-18 19:14:23 +0400

    Add unit-tests project; simple tests for distance

---
+++ SimpleGeometryProject/SimpleGeometryUnitTests/DistanceUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SimpleGeometry;
+
+namespace TestSimpleGeometry
+{
+    [TestClass]
+    public class DistanceUnitTest
+    {
+        /// <summary>
+        /// Константа для сравнения вещественных чисел на равенство
+        /// </summary>
+        public const double DOUBLE_EPSILON = 0.000001;
+
+        [TestMethod]
+        public void TestPointDistance()
+        {
+            Assert.AreEqual(3.5, GeometryCalcs.Distance(new Point(0, 0), new Point(3.5, 0)),
+                "test dist #1");
+            Assert.AreEqual(4.75, GeometryCalcs.Distance(new Point(-1, -3), new Point(-1, 1.75)),
+                "test dist #2");
+        }
+
+        [TestMethod]
+        public void TestCoordsDistance()
+        {
+            Assert.AreEqual(26.121783, GeometryCalcs.Distance(-1.5, -7, -25.345, -17.666), DOUBLE_EPSILON,
+                "test dist coord #1");
+            Assert.AreEqual(5, GeometryCalcs.Distance(-2, 1, 2, -2),
+                "test dist coord #2");
+        }
+    }
+}
---
+++ SimpleGeometryProject/SimpleGeometryUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("SimpleGeometryUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MICROSOFT")]
+[assembly: AssemblyProduct("SimpleGeometryUnitTests")]
+[assembly: AssemblyCopyright("Copyright © MICROSOFT 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Задание значения false для атрибута ComVisible приведет к тому, что типы из этой сборки станут невидимыми 
+// для COM-компонентов.  Если к одному из типов этой сборки необходимо обращаться из 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Если данный проект доступен для модели COM, следующий GUID используется в качестве идентификатора библиотеки типов
+[assembly: Guid("3845b8be-ed39-47de-9edd-3b71f25e4677")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
